f7bd470e0e98eb0d32f689bd2ec770d4e1e8da8f
charlespwd <charles.pclermont@gmail.com>
2013-09-16 16:18:55 -0400

    inlined get_rxy in construct_R for performance, reduces combined self times from .12s -> .9s, i.e. 25% improvement.

--- construct_R.f90
+++ construct_R.f90
@@ -4,23 +4,33 @@
 ! inputs:
 !  R: the correlation matrix, NxN
 !	theta: the correlation factor;
-! 	snap_pos: the vector of snapshot positions
+! 	X: the vector of snapshot positions
 !  D: # of dimensions
 !  Ns: # of snapshots
 !  Pc: Power of correlation 
 !
 ! outputs: 
 ! 	R	 
-SUBROUTINE construct_R(R,theta,snap_pos,D,Ns,Pc)
+SUBROUTINE construct_R(R,theta,X,D,Ns,Pc)
    IMPLICIT NONE
    DOUBLE PRECISION :: get_rxy
    INTEGER :: D, Ns, Pc
-   INTEGER :: ii, jj
+   INTEGER :: ii, jj, dd
    DOUBLE PRECISION :: R(Ns,Ns),theta(D)
-   DOUBLE PRECISION :: snap_pos(Ns,D)
+   DOUBLE PRECISION :: X(Ns,D)
+   DOUBLE PRECISION :: tmp
    DO ii=1,Ns
       DO jj=ii,Ns
-         R(ii,jj) = get_rxy(theta,snap_pos(ii,:),snap_pos(jj,:),D,Pc)
+         ! inlined for performance, this gets called so much that the overhead
+         ! isn't worth the better readability. I left the call there
+         ! instead
+!         R(ii,jj) = get_rxy(theta,X(ii,:),X(jj,:),D,Pc)
+         tmp = 0.0d0
+         DO dd=1,D
+            tmp = tmp &
+               - theta(dd) * (abs(X(ii,dd) - X(jj,dd)) ** Pc)
+         END DO
+         R(ii,jj) = exp(tmp)
          R(jj,ii) = R(ii,jj)
       END DO
    END DO
