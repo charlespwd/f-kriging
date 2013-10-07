1e49cf46f3722dac7711128ed834cbb7acf4f4fd
charlespwd <charles.pclermont@gmail.com>
2013-10-07 14:36:33 -0400

    moved functions in analyticalfunctions

--- analytical_functions.f90
+++ analytical_functions.f90
@@ -39,9 +39,83 @@ MODULE ANALYTICAL_FUNCTIONS
       END SELECT
    END FUNCTION
 
-   INCLUDE 'fdrag.f90'
-   INCLUDE 'fbranin.f90'
-   INCLUDE 'fcosine.f90'
+   FUNCTION fdrag(xv,D)
+      USE PARAMS, ONLY : PI
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: D
+      DOUBLE PRECISION, INTENT(IN) :: xv(1,D)
+      DOUBLE PRECISION, DIMENSION(1,1+2) :: fdrag
+      DOUBLE PRECISION :: x,y
+      ! D should be two
+      IF (D /= 2) THEN
+         print *, 'D should be 2'
+         STOP
+      END IF
+
+      x = xv(1,1)
+      y = xv(1,2)
+
+      ! y
+      fdrag(1,1) =  0.001d0*(1+y**2 /(1.08d0-y)) *x;
+      ! gradx
+      fdrag(1,2) =  0.001d0 *(1+y**2 /(1.08d0-y));
+      ! grady
+      fdrag(1,3) =  0.001d0 * x  * (2 * y  / (1.08d0 - y) + y**2  / ((1.08d0-y)**2));
+   END FUNCTION
+
+   ! fbranin(x,D) 
+   ! returns the function y and the gradient of y @ x
+   !  arguments:
+   !  x: loc vector, [1,D]
+   !  D: # of dimensions of domain, 
+   !  fbranin: [y,gradx,grady], [D+1,1]
+   FUNCTION fbranin(xv,D)
+      USE PARAMS, ONLY : PI
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: D
+      DOUBLE PRECISION, INTENT(IN) :: xv(1,D)
+      DOUBLE PRECISION, DIMENSION(1,1+2) :: fbranin
+      DOUBLE PRECISION :: x,y
+      ! D should be two
+      IF (D /= 2) THEN
+         print *, 'D should be 2'
+         STOP
+      END IF
+
+      x = xv(1,1)
+      y = xv(1,2)
+
+      ! y
+      fbranin(1,1) =  (y - 5.1d0/(4*PI**2) * x**2 + 5.d0/PI * x - 6)**2 + 10 * (1 - 1.0d0/(8*PI))*cos(x) + 10
+      ! gradx
+      fbranin(1,2) =  2 * (5.d0/PI - 0.258369d0*x) * (-6 + 5*x/PI - 0.129185d0*x**2 + y) - 10 * (1-1.0d0/(8*PI)) * sin(x)
+      ! grady
+      fbranin(1,3) = 2*(-6 + 5.d0*x/pi - 0.129185d0*x**2 + y)
+   END FUNCTION   
+   
+   FUNCTION fcosine(xv,D)
+      USE PARAMS, ONLY : PI
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: D
+      DOUBLE PRECISION, INTENT(IN) :: xv(1,D)
+      DOUBLE PRECISION, DIMENSION(1,1+2) :: fcosine
+      DOUBLE PRECISION :: x,y
+      ! D should be two
+      IF (D /= 2) THEN
+         print *, 'D should be 2'
+         STOP
+      END IF
+
+      x = xv(1,1)
+      y = xv(1,2)
+
+      ! y
+      fcosine(1,1) =  cos(10*x) + sin(10*y) + x*y
+      ! gradx
+      fcosine(1,2) =  -10*sin(10*x) + y
+      ! grady
+      fcosine(1,3) =  10*cos(10*y) + x;
+   END FUNCTION
 END MODULE
 
 !PROGRAM p
