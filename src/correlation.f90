38bbdfbdf292556f6efd7786003ae56da78a6b26
charlespwd <charles.pclermont@gmail.com>
2013-10-12 13:39:49 -0400

    unneccessary memory allocation in inverseR, and nugget

--- src/correlation.f90
+++ src/correlation.f90
@@ -109,17 +109,13 @@ module correlation
          USE F95_LAPACK,ONLY:LA_GESVX
          IMPLICIT NONE 
          INTEGER, INTENT(IN) :: Ns
-         DOUBLE PRECISION, INTENT(IN) :: I(Ns,Ns)
+         DOUBLE PRECISION, INTENT(INOUT) :: I(Ns,Ns)
          DOUBLE PRECISION, INTENT(INOUT) :: R(Ns,Ns)
          DOUBLE PRECISION, INTENT(OUT) :: Rinv(Ns,Ns)
-         DOUBLE PRECISION :: wR(Ns,Ns), wI(Ns,Ns)
          DOUBLE PRECISION :: RCOND
          INTEGER :: INFO
 
-         wR = R ! we do not want R and I to be modified by LAPACK
-         wI = I
-
-         CALL LA_GESVX(wR,wI,Rinv,RCOND=RCOND,INFO=INFO)
+         CALL LA_GESVX(R,I,Rinv,RCOND=RCOND,INFO=INFO)
          CALL NUGGETCORRECT(R,I,Rinv,Ns,RCOND ** (-1))
       END SUBROUTINE
 
@@ -134,8 +130,7 @@ module correlation
          IMPLICIT NONE
          INTEGER,INTENT(IN) :: Ns
          DOUBLE PRECISION,INTENT(inout) :: Rinv(Ns,Ns), R(Ns,Ns) 
-         DOUBLE PRECISION,INTENT(in) :: I(Ns,Ns) 
-         DOUBLE PRECISION :: wI(Ns,Ns),wR(Ns,Ns)
+         DOUBLE PRECISION,INTENT(inout) :: I(Ns,Ns) 
          INTEGER :: ii,info
          DOUBLE PRECISION :: eps, cond,rcon
 
@@ -151,10 +146,8 @@ module correlation
             END DO
 
             ! we do not want R or I to be modified by the LAPACK subroutines
-            wR = R
-            wI = I
 
-            CALL LA_GESVX(wR,wI,Rinv,RCOND=rcon,INFO=info) 
+            CALL LA_GESVX(R,I,Rinv,RCOND=rcon,INFO=info) 
       !      WRITE(*,'(a25,E11.5)') 'cond after nugget= ', rcon**(-1)
          END IF
       END SUBROUTINE
